bb13e0964dc73479d528709dfaa35f708ccb4aab
samrat369 <45921584+samrat369@users.noreply.github.com>
2023-10-29 13:55:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const proxy = httpProxy.createProxyServer({});
 // Create an HTTP server that will handle proxy requests
 const server = http.createServer((req, res) => {
   // Proxy all requests to http://localhost:3000
-  proxy.web(req, res, { target: 'http://localhost:3000' });
+  proxy.web(req, res, { target: 'https://mukul1098.pythonanywhere.com/' });
 });
 
 // Listen for incoming requests on port 8000
-const port = 8000;
+const port = 3000;
 server.listen(port, () => {
   console.log(`Proxy server is running on port ${port}`);
 });
